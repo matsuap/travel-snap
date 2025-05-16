b3d1073ee820bdf665b829c430a7c8e7ee18c8ef
MATSUMURA Taishi <taishi.matsumura@at-peak.com>
2025-05-17 05:57:16 +0900

    fix: remove unused updateSW declaration in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { registerSW } from 'virtual:pwa-register'
+console.log('import.meta.env =', import.meta.env);
+
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
