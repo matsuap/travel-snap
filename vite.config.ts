b05a8a1c398b5aed0869d73e9d35ca562d44e16c
MATSUMURA Taishi <taishi.matsumura@at-peak.com>
2025-05-17 05:51:01 +0900

    chore: allow ngrok host in Vite server allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  server: {
+    allowedHosts: ['0b00-111-90-111-49.ngrok-free.app']
+  },
   plugins: [
     react(),
     VitePWA({
