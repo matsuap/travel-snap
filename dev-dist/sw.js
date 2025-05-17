1dfbe34c42fb308674a3cc037029591ff46d4ea1
MATSUMURA Taishi <taishi.matsumura@at-peak.com>
2025-05-17 14:29:10 +0900

    fix: update revision for precached index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.urd63epa88g"
+    "revision": "0.hsogmg09ms8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
